49df0ccb0b27e51625899c09c5b199b929c0c329
Jeff Otieno <jeffotieno@example.com>
2025-01-10 18:19:34 +0300

    Update frontend build and fix dependencies

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import productReducer from './slices/productSlice';
-import cartReducer from './slices/cartSlice';
 import authReducer from './slices/authSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
-    products: productReducer,
-    cart: cartReducer,
     auth: authReducer,
   },
 });
+
+export default store;
